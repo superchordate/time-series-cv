4159a8a95e284291c6892a4a8755de9ff7a63fe1
bryce <superchordate@gmail.com>
2020-11-27 15:17:28 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,172 @@
+# Function for Time Series Cross-Validation
+
+Easily compare multiple time series models using sliding and/or expanding-window cross validation.
+
+## Usage
+
+Read the function with `source`, set up your models, and run the cross-validation:
+
+```r
+require(easyr)
+require(forecast)
+require(vars)
+begin()
+
+# read the function.
+source('cv-fn.R')
+
+# read data and remove your holdout.
+# data is from https://archive.ics.uci.edu/ml/datasets/Appliances+energy+prediction
+# Luis M. Candanedo, Veronique Feldheim, Dominique Deramaix, Data driven prediction models of energy use of appliances in a low-energy house, Energy and Buildings, Volume 140, 1 April 2017, Pages 81-97, ISSN 0378-7788
+dt = read.any('daily.csv')
+test = tail(dt, 7)
+train = head(dt, nrow(dt) - 7)
+
+# set up models.
+models = list(
+    
+    # add an entry to this list for each model.
+    # example:
+    list(
+      enabled = FALSE,
+      # give your model a name to be used in charts, etc.
+      name = 'ARIMA(1,0,1)[0,1,2][24]',
+      
+      # function to fit the model. 
+      #   in: y (time series), ... (unused inputs, add this for flexibility)
+      #   out: model object
+      fit = function(y, ...){
+        m = Arima(
+          y,
+          order = c(1, 0, 1),
+          seasonal = list(order = c(0, 1, 2), period = 24)
+        )
+        return(m)
+      }
+      
+      # function to forecast.
+      #   in: m (output from fit), h (forecast horizon)
+      #   out: point forecasts (no confidence intervals)
+      #   if left blank will use: function(object, h, ...) forecast(object, h)$mean
+      
+    ),
+  
+    # VAR(2)
+    list(
+      name = 'VAR(2)',
+      fit = function(y, xreg){
+        VAR(ts(cbind(y, xreg$out_temperature), frequency = 7), p = 2, type = "both", season = 7)
+      },
+      forecast = function(m, h, ...) head(data.frame(forecast(m, h = h, frequency = 7))$Point.Forecast, h),
+      residuals = function(m) residuals(m)[, 'y']
+    ),
+    
+    # VAR(3)
+    list(
+      name = 'VAR(3)',
+      fit = function(y, xreg){
+        VAR(ts(cbind(y, xreg$out_temperature), frequency = 7), p = 3, type = "both", season = 7)
+      },
+      forecast = function(m, h, ...) head(data.frame(forecast(7))$Point.Forecast, h),
+      residuals = function(m) residuals(m)[, 'y']
+    ),
+    
+    # Fourier (K = 2)
+    list(
+      name = 'Fourier (K = 2)',
+      fit = function(y, xreg){
+        Arima(          
+          ts(y, frequency = 7),
+          xreg = fourier(x = ts(y, frequency = 7), K = 2)
+        )
+      },
+      forecast = function(m, h, y, ...){
+        forecast(m, xreg = fourier(x = ts(y, frequency = 7), K = 2, h = h))$mean
+      }
+    ),
+    
+    # Fourier (K = 3)
+    list(
+      name = 'Fourier (K = 3)',
+      fit = function(y, xreg){
+        Arima(          
+          ts(y, frequency = 7),
+          xreg = fourier(x = ts(y, frequency = 7), K = 3)
+        )
+      },
+      forecast = function(m, h, y, ...){
+        forecast(m, xreg = fourier(x = ts(y, frequency = 7), K = 3, h = h))$mean
+      }
+    ),
+    
+    # tbats
+    list(
+      name = 'tbats',
+      fit = function(y, xreg){
+        tbats(ts(y, frequency = 7))
+      },
+      bic = function(m) as.numeric(NA) # BIC is not implemented for TBATS
+    ),
+    
+    # Reg w/ ARIMA errors
+    list(
+      name = 'Reg w/ ARIMA errors',
+      fit = function(y, xreg){
+        xreg$is_weekend = 1 * xreg$is_weekend
+        Arima(
+          ts(y, frequency = 7),
+          order = c(0,0,1),
+          seasonal = list(order = c(1,0,0), period = 7),
+          xreg = as.matrix(xreg[, c('out_temperature', 'is_weekend')])
+        )
+      },
+      forecast = function(m, h, xreg, ...){
+        xreg$is_weekend = 1 * xreg$is_weekend
+        forecast(
+          m, 
+          h = h, 
+          # repeat last row h times.
+          xreg = as.matrix(xreg[ rep(length(xreg), h), ][, c('out_temperature', 'is_weekend')])
+        )$mean
+        
+      }
+    ),
+    
+    # ETS
+    list(
+        name = 'ets(model = "MAM")',
+        fit = function(y, ...){
+            m = ets(ts(y, frequency = 24), model = 'MAM')
+            return(m)
+        }
+    )
+)
+
+# run the CV.
+cv = modelcv(
+    y = train$y,
+    xreg = train[, c('out_temperature', 'is_weekend')],
+    models = models,
+    windowsize = 7 * 3,
+    h = 7,
+    numcvs = 10,
+    runtypes = c('sliding', 'expanding'),
+    numcores = 8
+)
+
+# summarize your output.
+cv$summary %>%
+  group_by(model, window) %>% 
+  summarize(
+    forecast_horizons = length(forecast_horizon),
+    cv_count = mean(cv_count),
+    mae_fit = mean(mae_fit), 
+    mae_test = mean(mae_test), 
+    rmse_fit = mean(rmse_fit), 
+    rmse_test = mean(rmse_test), 
+    mean_bic = mean(mean_bic)
+  ) %>%
+  arrange(rmse_test) %>% View
+
+```
+
---
+++ cv-fn.R
@@ -0,0 +1,147 @@
+require(glue)
+require(dplyr)
+require(doSNOW)
+
+# create a list with information for all the models.
+modelcv = function(
+    y, # time series
+    xreg = NULL, # xreg variables to pass to model fitting.
+    models, # list with model functions.
+    windowsize = 160, # width of sliding window (and min expanding window).
+    h = 12, # forecast horizon (number of periods to forecast).
+    numcvs = NULL, # number of CVs to run. CVs will use the latest possible data.
+    runtypes = c('sliding', 'expanding'), # types of CVs to run.
+    verbose = TRUE, # print information about run status.
+    numcores = 2, # number of parallel processes to use. set to 1 for easier troubleshooting.
+    packages = c(), # add'l packages required for your models. will be passed to parallel processes.
+    ... # other args that will be passed to modeling functions.
+){
+    
+    # calculating where to start is a bit tricky.
+    #  first we calculate what the last training window will be (depends on forecast interval h)
+    #  then we subtract the number of cvs from that to get our starting point
+    lastwindow = (length(y) - h - windowsize + 1):(length(y) - h)
+    startat = if(is.null(numcvs)){ 1 }  else { lastwindow[1] - numcvs + 1 }
+    iterateoverstarts = startat:lastwindow[1]
+    
+    if(!is.null(numcvs) && windowsize + numcvs + h > length(y)) stop(glue('
+       windowsize + numcvs + h ({windowsize} + {numcvs} + {h} = {windowsize + numcvs + h} exceeds number of observations ({length(y)})) 
+    '))
+
+    # build the list of CV info.
+    dt = list()
+    for(iwindowstart in iterateoverstarts){
+
+        # expanding window.        
+        if('expanding' %in% runtypes) for(imodel in models) if(is.null(imodel$enabled) || imodel$enabled){
+
+            idt = list(
+                train_from = 1,
+                train_thru =(iwindowstart + windowsize - 1),
+                test_from = (iwindowstart + windowsize),
+                test_thru = (iwindowstart + windowsize + h - 1)
+            )
+            idt$train = y[idt$train_from:idt$train_thru]
+            idt$test = y[idt$test_from:idt$test_thru]
+            if(!is.null(xreg)) idt$xreg = xreg[idt$train_from:idt$train_thru, ]
+
+            idt$model = imodel
+            idt$windowtype = 'expanding'            
+
+            dt[[length(dt) + 1]] <- idt
+            rm(idt)
+
+        }
+
+        if('sliding' %in% runtypes) for(imodel in models) if(is.null(imodel$enabled) || imodel$enabled){
+
+            idt = list(
+                train_from = iwindowstart,
+                train_thru =(iwindowstart + windowsize - 1),
+                test_from = (iwindowstart + windowsize),
+                test_thru = (iwindowstart + windowsize + h - 1)
+            )
+            
+            idt$train = y[idt$train_from:idt$train_thru]
+            idt$test = y[idt$test_from:idt$test_thru]
+            if(!is.null(xreg)) idt$xreg = xreg[idt$train_from:idt$train_thru, ]
+            
+            idt$model = imodel
+            idt$windowtype = 'sliding'
+
+            dt[[length(dt) + 1]] <- idt
+            rm(idt)
+
+        }
+
+    }
+
+    # set up the function to use at each iteration.
+    dofn = function(idt){
+
+            im = idt$model$fit(y = idt$train, xreg = idt$xreg)
+            h = length(idt$test)
+
+            if(is.null(idt$model$forecast)) idt$model$forecast = function(m, h, ...) forecast(m, h)$mean
+            if(is.null(idt$model$residuals)) idt$model$residuals = function(m) residuals(m)
+            if(is.null(idt$model$bic)) idt$model$bic = function(m) BIC(m)
+
+            data.frame(
+                window = idt$windowtype,
+                model = idt$model$name,
+                train_from = idt$train_from,
+                train_thru = idt$train_thru,
+                test_from = idt$test_from,
+                test_thru = idt$test_thru,
+                bic = idt$model$bic(im), # more consinstenlty implemented than AICc,
+                forecast_horizon = 1:h,
+                forecasterr = as.numeric(idt$model$forecast(m = im, h = h, xreg = idt$xreg, y = idt$train)) - idt$test,
+                forecast = list(as.numeric(idt$model$forecast(m = im, h = h, xreg = idt$xreg, y = idt$train))),
+                test = list(idt$test),
+                modelerr = as.numeric(tail(idt$model$residuals(im), h))
+            )        
+
+    }
+
+    # run the CVs in parallel.
+    if(verbose) cat(glue('Running {length(dt)} CV models \n'))
+    if(is.null(numcores) || numcores == 1){
+        # if 1 core, just use lapply for easier troubleshooting.
+        results = lapply(dt, dofn)
+    } else {
+        cat('\t in parallel')
+        cl = makeSOCKcluster(numcores)
+        registerDoSNOW(cl)
+        results = tryCatch({
+            foreach(
+                i = dt, 
+                .packages = c('forecast', 'vars', 'glue', packages),
+                .options.snow = if(verbose) list(progress = function(n) if(n %% 10 == 0) cat("\rCV ", n, " of ", length(dt), " complete \n"))
+            ) %dopar% dofn(i)
+        # on error, stop the cluster.
+        }, error = function(e){
+            stopCluster(cl)
+            stop(e)
+        })
+        stopCluster(cl)
+    }
+
+    # combine into a single data frame.
+    results = bind_rows(results)
+    
+    return(list(
+        results = results, 
+        summary = results %>%
+            group_by(model, window, forecast_horizon) %>% 
+            summarize(
+                cv_count = n(),
+                mae_fit = mean(abs(modelerr)),
+                mae_test = mean(abs(forecasterr)),
+                rmse_fit = sqrt(mean(modelerr ** 2)),
+                rmse_test = sqrt(mean(forecasterr ** 2)),
+                mean_bic = mean(bic)
+            ) %>%
+            arrange(forecast_horizon, rmse_test)
+    ))
+
+}
